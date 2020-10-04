d27922744abcc850958cb735ccb646bea48b9623
Pranav1710 <pranavchanpara007@gmail.com>
2020-10-04 15:08:49 +0530

    04-10-2020

--- src/components/contentpage/ProductSidebar.js
+++ src/components/contentpage/ProductSidebar.js
@@ -1,72 +1,102 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./sidebar.css";
 export class ProductSidebar extends React.Component {
-  constructor(){
+  constructor() {
     super();
     // this.state({type: this.props.type});
-
   }
-  componentDidMount(){
-    if(this.props.type!=='')
-      this.dropdownOpen(this.props.type);
+  componentDidMount() {
+    if (this.props.type !== "") this.dropdownOpen(this.props.type);
   }
-  dropdownOpen = (type)=>{
-      
+  dropdownOpen = (type) => {
     let element = document.getElementById(type);
-    if(element.classList.contains('activeItem')){
-      element.classList.remove('activeItem');
-    } else{
-      element.classList.add('activeItem');
+    if (element.classList.contains("activeItem")) {
+      element.classList.remove("activeItem");
+    } else {
+      element.classList.add("activeItem");
     }
-  }
-  render(){
-    const dropdown = (type)=>{
-      let t = type=='wall'?'wall':type=='floor'?'floor':type=='parking'?'parking':type=='dc'?'dc':'pg';
+  };
+  render() {
+    const dropdown = (type) => {
+      let t =
+        type == "wall"
+          ? "wall"
+          : type == "floor"
+          ? "floor"
+          : type == "parking"
+          ? "parking"
+          : type == "dc"
+          ? "dc"
+          : "pg";
       console.log(t);
       this.dropdownOpen(t);
-    }
-    
+    };
+
     return (
       <div className="productSide">
         <aside className="sidebar">
-          <button className="btn-inq">
-            <i className="fa fa-envelope env-btn" aria-hidden="true"></i>
-            Inquiry
-          </button>
-          <div className="product-type" id='wall' >
-            <h5 className="product-title" onClick={dropdown.bind(this,'wall')}>
+          <Link to="/Contact-Us">
+            <button className="btn-inq">
+              <i className="fa fa-envelope env-btn" aria-hidden="true"></i>
+              Inquiry
+            </button>
+          </Link>
+          <div className="product-type" id="wall">
+            <h5 className="product-title" onClick={dropdown.bind(this, "wall")}>
               Wall Tiles
-              <i className="fa fa-angle-right angle-right" aria-hidden="true"></i>
+              <i
+                className="fa fa-angle-right angle-right"
+                aria-hidden="true"
+              ></i>
             </h5>
-            <ul className="product-items" >
+            <ul className="product-items">
               <li className="product-item wall-item">300 x 450</li>
               <li className="product-item wall-item">300 x 600</li>
             </ul>
           </div>
-  
-          <div className="product-type" id='parking'>
-            <h5 className="product-title" onClick={dropdown.bind(this,'parking')}>
+
+          <div className="product-type" id="parking">
+            <h5
+              className="product-title"
+              onClick={dropdown.bind(this, "parking")}
+            >
               parking Tiles
-              <i className="fa fa-angle-right angle-right" aria-hidden="true"></i>
+              <i
+                className="fa fa-angle-right angle-right"
+                aria-hidden="true"
+              ></i>
             </h5>
             <ul className="product-items">
               <li className="product-item">300 x 300</li>
               <li className="product-item">400 x 400</li>
             </ul>
           </div>
-  
-          <div className="product-type" id='floor' >
-            <h5 className="product-title" onClick={dropdown.bind(this,'floor')}>
+
+          <div className="product-type" id="floor">
+            <h5
+              className="product-title"
+              onClick={dropdown.bind(this, "floor")}
+            >
               floor Tiles
-              <i className="fa fa-angle-right angle-right" aria-hidden="true"></i>
+              <i
+                className="fa fa-angle-right angle-right"
+                aria-hidden="true"
+              ></i>
             </h5>
             <ul className="product-items">
               <li className="product-item">Nano(Soluble Salt)</li>
               <li className="product-item">Porceline: 600 x 600</li>
-              <li className="product-sub-type" id='dc'>
-                <h6 className="product-sub-title" onClick={dropdown.bind(this,'dc')}>
+              <li className="product-sub-type" id="dc">
+                <h6
+                  className="product-sub-title"
+                  onClick={dropdown.bind(this, "dc")}
+                >
                   Double Charge
-                  <i className="fa fa-angle-right angle-right" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-angle-right angle-right"
+                    aria-hidden="true"
+                  ></i>
                 </h6>
                 <ul className="product-sub-items">
                   <li className="product-sub-item">600 x 600</li>
@@ -74,10 +104,16 @@ export class ProductSidebar extends React.Component {
                   <li className="product-sub-item">600 x 1200</li>
                 </ul>
               </li>
-              <li className="product-sub-type" id='pg'>
-                <h6 className="product-sub-title" onClick={dropdown.bind(this,'pg')}>
+              <li className="product-sub-type" id="pg">
+                <h6
+                  className="product-sub-title"
+                  onClick={dropdown.bind(this, "pg")}
+                >
                   Gvt/Pgvt
-                  <i className="fa fa-angle-right angle-right" aria-hidden="true"></i>
+                  <i
+                    className="fa fa-angle-right angle-right"
+                    aria-hidden="true"
+                  ></i>
                 </h6>
                 <ul className="product-sub-items">
                   <li className="product-sub-item">600 x 600</li>
@@ -86,16 +122,19 @@ export class ProductSidebar extends React.Component {
               </li>
             </ul>
           </div>
-  
+
           <div className="product-type">
             <h5 className="product-title">
               Sanatery ware
-              <i className="fa fa-angle-right angle-right" aria-hidden="true"></i>
+              <i
+                className="fa fa-angle-right angle-right"
+                aria-hidden="true"
+              ></i>
             </h5>
           </div>
         </aside>
       </div>
     );
   }
-};
+}
 export default ProductSidebar;
