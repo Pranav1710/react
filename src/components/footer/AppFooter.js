d55f64fdab88be1b0a80564caca10a20e660fa1c
Pranav1710 <pranavchanpara007@gmail.com>
2020-10-12 10:48:51 +0530

    2020-10-12 Footer complate

--- src/components/footer/AppFooter.js
+++ src/components/footer/AppFooter.js
@@ -1,10 +1,103 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./footer.css";
 export default function AppFooter() {
   return (
     <footer className="footer">
-      <div className="container">
-        <span className="text-muted">© Copyright 2020 Blue International</span>
+      <div className="main-foot">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <h6>Blue International</h6>
+              <hr />
+              <ul className="company-info">
+                <li style={{ display: "flex" }}>
+                  <i
+                    className="fa fa-map-marker map-mark"
+                    aria-hidden="true"
+                  ></i>
+                  <p>
+                    Chitrakut-5, <br />
+                    b/h g.i.d.c, <br />
+                    sanala road, <br />
+                    morbi,gujarat,india-363641 <br />
+                  </p>
+                </li>
+                <li>
+                  <i className="fa fa-phone" aria-hidden="true"></i>
+                  <span>+91 9824665321</span>
+                </li>
+                <li>
+                  <i className="fa fa-envelope" aria-hidden="true"></i>
+                  <span>Blue@blueinternationl.com</span>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <h6>Quick Links</h6>
+              <hr />
+              <ul className="quick-links">
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/exports">
+                    Exports
+                  </Link>
+                </li>
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/company">
+                    Company
+                  </Link>
+                </li>
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/contact-us">
+                    Contact-us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <h6>Our Products</h6>
+              <hr />
+              <ul className="products">
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/products/wall-tiles">
+                    Wall Tiles
+                  </Link>
+                </li>
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/products/floor-tiles">
+                    Floor Tiles
+                  </Link>
+                </li>
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/products/parking-tiles">
+                    Parking Tiles
+                  </Link>
+                </li>
+                <li>
+                  <i className="fa fa-caret-right" aria-hidden="true"></i>
+                  <Link className="footer-link" to="/products/sanetery-ware">
+                    Sanatery Ware
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-muted">
+        <p className="container">© Copyright 2020 Blue International | Developed by <span>Pranav Chanpara</span></p>
       </div>
     </footer>
   );
