ba4e5c36be1dd992d8c988e20d070d94323fe664
Pranav1710 <pranavchanpara007@gmail.com>
2020-10-17 15:24:44 +0530

    deploy

--- src/config.js
+++ src/config.js
@@ -6,9 +6,4 @@ export const imgs = {
     img3 : `${process.env.PUBLIC_URL}/assets/imgs/3.jpg`
 };
 
-export const products = {
-    img1 : `${process.env.PUBLIC_URL}/assets/imgs/products/1.jpg`,
-    img2 : `${process.env.PUBLIC_URL}/assets/imgs/products/2.jpg`,
-    img3 : `${process.env.PUBLIC_URL}/assets/imgs/products/3.jpg`
-};
-// export const Products = `${process.env.PUBLIC_URL}/assets/data/products.json`;
+export const clientKey = '6LdK19YZAAAAAIgO-fQS2GjP2Eklr63aajQjrjaJ';
